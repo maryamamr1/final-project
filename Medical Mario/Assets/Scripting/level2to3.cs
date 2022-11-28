557ceca5ac79fcaaba707a06b8f1cdace56e905c
maryamamr1 <maryamamr251@gmail.com>
2022-11-27 20:44:41 -0500

    Several New Scenes
    Added game over scene and set up 2 levels.

---
+++ Medical Mario/Assets/Scripting/level2to3.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level2to3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("level3");
+        }
+    }
+}
+
+//Followed tutorial for this code using https://www.youtube.com/watch?v=nPigL-dIqgE&t=1949s&ab_channel=bblakeyyy
